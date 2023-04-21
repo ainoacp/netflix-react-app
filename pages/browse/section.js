a3312fdc44226a2f453b18c1209c1da32a29829f
Ainoa Couto <ainhoacp_94@msn.com>
2023-04-21 14:00:53 +0100

    landing page styles

--- pages/browse/section.js
+++ pages/browse/section.js
@@ -1,23 +1,31 @@
 import N from '../../public/images/N.svg'
 import Top from '../../public/images/Top10.svg'
 
-const Section = ({ heroMovie }) => {
+const Section = ({heroMovie}) => {
+
+  console.log(heroMovie);
+
   return (
-    <div className="text-white max-w-fit p-8 mt-20 z-10">
+    <div className="text-white border-white w-1/2 max-h-xs p-8 mt-14 z-10">
       <div className='flex items-center'>
-        <div className='mr-6'>
+        <div className='mr-6 mb-6 gap-4 flex items-center'>
           <N/>
-        </div>
-        <div>
-          <p className='text-3xl text-slate-300 font-bold tracking-widest'>MOVIES</p>
+          <div>
+          <p className='text-xl text-slate-300 font-semibold tracking-widest'>MOVIES</p>
+          </div>
         </div>
       </div>
-      <div className='font-serif text-9xl w1/2 my-8'>{heroMovie?.title}</div>
+      <div className='font-serif text-8xl mt-8 mb-8'>
+        {heroMovie?.title}
+      </div>
       <div className='flex items-center gap-3 mb-6'>
         <Top/>
-        <p>#1 in TV Movies Today</p>
+        <p className='text-xl'>#1 in TV Movies Today</p>
       </div>
-      <div className='text-xs w-1/2'>{heroMovie?.overview}</div>
+      <div className='text-xs'>
+        {heroMovie?.overview}
+      </div>
+      <div></div>
       <div>
 
       </div>
